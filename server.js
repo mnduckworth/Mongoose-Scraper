0678fc4a6a3fb0cff9c3860f8d15d8b4b126066b
Matthew Duckworth <matthewduckworth100@gmail.com>
2017-12-02 20:14:45 -0600

    maybe actually done

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose.connect("mongodb://heroku_2tglz8p2:nee0kjvcp45r9ukpbr5vev2qs9@ds127936.
 // Routes
 
 app.get("/", function (req, res) {
-    res.render("index");
+    res.redirect("/scrape");
 });
 
 // A GET route for scraping the echojs website
